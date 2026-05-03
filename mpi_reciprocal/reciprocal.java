f1f575eba28864092063e39b331ef4eb3b0c9b79
SamaliRajderkar <sgrajderkar@gmail.com>
2026-05-03 21:17:06 +0530

    reciprocal

--- mpi_reciprocal/reciprocal.java
+++ mpi_reciprocal/reciprocal.java
@@ -11,40 +11,33 @@ public class reciprocal {
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
 
-        int[]    data        = new int[size];
-        int[]    recv_buf    = new int[1];
-        double[] local_recip = new double[1];
-        double[] result      = new double[size];   // gathered at root
+        int[] data = new int[size];
+        int[] recv = new int[1];
+        double[] result = new double[size];
+        double[] temp = new double[1];
 
+        // Root initializes data
         if (rank == 0) {
-            System.out.println("Root process initialising array:");
             for (int i = 0; i < size; i++) {
-                data[i] = (i + 1) * 10;           // 10, 20, 30, 40
-                System.out.print(data[i] + " ");
+                data[i] = (i + 1) * 10;
             }
-            System.out.println();
         }
 
-        // Scatter one element per process
+        // Distribute one element to each process
         MPI.COMM_WORLD.Scatter(data, 0, 1, MPI.INT,
-                               recv_buf, 0, 1, MPI.INT, 0);
+                               recv, 0, 1, MPI.INT, 0);
 
-        int val = recv_buf[0];
+        // Compute reciprocal
+        temp[0] = 1.0 / recv[0];
 
-        // Each process computes reciprocal
-        local_recip[0] = 1.0 / val;
-        System.out.println("Process " + rank + " | Value: " + val
-                         + " | Reciprocal: " + local_recip[0]);
+        // Gather results at root
+        MPI.COMM_WORLD.Gather(temp, 0, 1, MPI.DOUBLE,
+                              result, 0, 1, MPI.DOUBLE, 0);
 
-        // Gather all reciprocals back to root
-        MPI.COMM_WORLD.Gather(local_recip, 0, 1, MPI.DOUBLE,
-                              result,      0, 1, MPI.DOUBLE, 0);
-
-        // Root displays the final resultant array
+        // Print final result at root
         if (rank == 0) {
-            System.out.println("\nResultant reciprocal array at root:");
             for (int i = 0; i < size; i++) {
-                System.out.printf("1/%d = %.4f%n", data[i], result[i]);
+                System.out.print(result[i] + " ");
             }
         }
 
@@ -54,16 +47,4 @@ public class reciprocal {
 
 // samalirajderkar@Samalis-MacBook-Air mpi_reciprocal % javac -cp .:/Users/samalirajderkar/mpj-v0_44/lib/mpj.jar reciprocal.java
 // samalirajderkar@Samalis-MacBook-Air mpi_reciprocal % /Users/samalirajderkar/mpj-v0_44/bin/mpjrun.sh -np 4 reciprocal         
-// MPJ Express (0.44) is started in the multicore configuration
-// Root process initialising array:
-// 10 20 30 40 
-// Process 0 | Value: 10 | Reciprocal: 0.1
-// Process 2 | Value: 30 | Reciprocal: 0.03333333333333333
-// Process 3 | Value: 40 | Reciprocal: 0.025
-// Process 1 | Value: 20 | Reciprocal: 0.05
-// Resultant reciprocal array at root:
-// 1/10 = 0.1000
-// 1/20 = 0.0500
-// 1/30 = 0.0333
-// 1/40 = 0.0250
-// samalirajderkar@Samalis-MacBook-Air mpi_reciprocal % 
+ 
